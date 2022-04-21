3fc211bbc2e2cc20966c351c0e12f9db37e228df
Milton Vandol Goh <miltonvandol@gmail.com>
2022-04-21 13:32:42 -0700

    Creating a function to search words horizontal and vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+/* Module 1, Week 5: Pair Programming - Word Search
+Partners: Gloria Cheung (Github: Gloria-Cheung) & Milton Vandol Goh (GitHub: Vandal19) */
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
 
-module.exports = wordSearch
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    const verticalJoin = [];
+    for (let i = 0; i <= letters.length - 1; i++) {
+        let newStr = "";
+        for (let j = 0; j <= letters[i].length; j++) {
+            newStr += letters[j][i];
+        }
+        verticalJoin.push(newStr);
+    }
+
+    for (let l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+    
+    return false;
+};
+
+
+module.exports = wordSearch;
